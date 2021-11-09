2eed56f3c112be29ab0abd957e06757c1d3aa459
SebasGarcia08 <segaracos@outlook.com>
2021-11-09 10:33:16 -0500

    Tried to do this test

--- tests/contact-us.test.js
+++ tests/contact-us.test.js
@@ -9,12 +9,12 @@ test('The contact us option is successfully', async (t) => {
     const message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porttitor commodo ex a ultrices. Suspendisse quis vulputate ante. Aenean maximus."
 
     // Act
-    await t.click("")
-    await t.click("")
-    await t.click(Selector("").withText(subjectHeading))
-    await t.typeText("", emailAdress)
-    await t.typeText("", message)
-    await t.click("")
+    await t.click("#contact-link")
+    await t.click("#id_contact")
+    await t.click(Selector("#id_contact").withText(subjectHeading))
+    await t.typeText("#email", emailAdress)
+    await t.typeText("#message", message)
+    await t.click("#submitMessage")
 
     // Assert
     await t.expect(Selector("#center_column p").textContent).eql("Your message has been successfully sent to our team.")
